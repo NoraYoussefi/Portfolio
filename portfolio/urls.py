604c4e9566828ff32cbf8f96a9dd7daf34e66c6f
Nora Youssefi <80409227+NoraYoussefi@users.noreply.github.com>
2022-04-16 01:16:42 +0000

    second commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,12 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views  
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # to include the urls declared on nora's project
+    path('',include('nora.urls')),   
     path('home/',views.home_view, name="home"),
     path('product/',views.product_view, name="product"),
 ]
+# this tells django where to find the media files 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
